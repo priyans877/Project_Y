ae34aec57675090faea7a85277a9a5e1e20ed277
priyans877 <pk87706@gmail.com>
2025-02-08 18:06:33 +0530

    Changes in bucket

--- Main/settings.py
+++ Main/settings.py
@@ -187,7 +187,7 @@ AWS_S3_REGION_NAME = os.getenv('AWS_S3_REGION_NAME')
 AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL =  None
 AWS_S3_VERIFY = True
-AWS_S3_CUSTOM_DOMAIN = '{}.s3.{}.amazonaws.com'.format(AWS_STORAGE_BUCKET_NAME ,AWS_S3_REGION_NAME)
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.ap-south-1.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 
 
 # DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
