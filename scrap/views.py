6a36d94ce5695dc55282e7eda27bc544ca610bb3
priyans877 <pk87706@gmail.com>
2025-02-12 15:29:49 +0530

    Error Resolve

--- scrap/views.py
+++ scrap/views.py
@@ -7,8 +7,6 @@ from selenium.webdriver.common.by import By
 from django.http import JsonResponse
 from django.contrib import messages
 from selenium import webdriver
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 from .models import *
 from .utils import *
 import tempfile
@@ -16,12 +14,14 @@ import json
 import os
 from django.conf import settings
 
+
+# Path to the media directory
 media_path = settings.MEDIA_ROOT
+
 
 driver = None
 
 def setup_driver():
-    global driver
     options = webdriver.ChromeOptions()
     options.add_argument('--ignore-certificate-errors')
     options.add_argument('--incognito')
@@ -29,22 +29,12 @@ def setup_driver():
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
     options.add_argument("--disable-gpu")
-    options.add_argument("--window-size=1920,1080")
-    options.binary_location = "/usr/bin/google-chrome"
+    # options.add_argument("--headless")  # Run in headless mode for servers
+    
+    # Set a unique temporary user data directory
     temp_user_data_dir = tempfile.mkdtemp()
     options.add_argument(f"--user-data-dir={temp_user_data_dir}")
-    try:
-        driver = webdriver.Chrome(options=options)
-        driver.set_page_load_timeout(30)
-        return driver
-    except Exception as e:
-        if driver:
-            try:
-                driver.quit()
-            except:
-                pass
-            driver = None
-        raise Exception(f"Failed to initialize driver: {str(e)}")
+    return webdriver.Chrome(options=options)
 
 def home(request):
     global driver
@@ -52,7 +42,9 @@ def home(request):
         driver.quit()
         driver = None
     request.session.pop('current_state', None)
+    
     return redirect('/profile/dashboard/')
+
 
 @login_required(login_url='login')
 def scraper_feed(request):
@@ -62,23 +54,31 @@ def scraper_feed(request):
         param_semester = request.POST['semester']
         param_batch = request.POST['batch']
         param_branch = request.POST['branch']
+        
         param_end = int(param_end)+1
+        # print(param_end)
+        
+        # Initialize the scraping session
         request.session['scraping_params'] = {
             'start_roll': param_start,
             'end_roll': param_end,
             'semester': param_semester,
             'current_roll': param_start,
-            'branch': param_branch,
-            'batch': param_batch,
-            'form_filled': False
+            'branch' : param_branch,
+            'batch' : param_batch,
+            'form_filled': False  # Track if we've filled the form
         }
-        return redirect('results', start_s=param_start, end_s=param_end, semester=param_semester, batch=param_batch, branch=param_branch)
+        
+        return redirect('results', start_s=param_start, end_s=param_end, semester=param_semester , batch =param_batch , branch=param_branch)
     return render(request, 'scrap/scrap_feed.html')
 
+driver = None
+
+
+
 @login_required(login_url='login')
-def run_scraper(request, start_s, end_s, semester, batch, branch):
+def run_scraper(request, start_s, end_s, semester , batch , branch):
     global driver
-<<<<<<< HEAD
     
     # Get current roll number from session
     current_state = request.session.get('current_state', {})
@@ -139,87 +139,35 @@ def run_scraper(request, start_s, end_s, semester, batch, branch):
     
     return render(request, 'scrap/result_scrap.html', {
         'form_data': form_entry,
-        'current_roll': current_roll, 
+        'current_roll': current_roll,  # Changed from start_s to 
         'image_url': image_url
     })
 
 
-=======
-    try:
-        current_state = request.session.get('current_state', {})
-        current_roll = current_state.get('roll_no', start_s)
-        if not driver:
-            driver = setup_driver()
-            driver.get("https://jcboseustymca.co.in/Forms/Student/ResultStudents.aspx")
-        wait = WebDriverWait(driver, 10)
-        roll_no_input = wait.until(EC.presence_of_element_located((By.ID, "txtRollNo")))
-        roll_no_input.clear()
-        roll_no_input.send_keys(current_roll)
-        semesters_list = ["First Semester", "Second Semester", "Third Semester", "Fourth Semester", "Fifth Semester", "Sixth Semester", "Seventh Semester", "Eighth Semester"]
-        dropdown = wait.until(EC.presence_of_element_located((By.ID, 'ddlSem')))
-        dropdown = Select(dropdown)
-        dropdown.select_by_visible_text(semesters_list[int(semester)-1])
-        driver.execute_script("window.open('');")
-        driver.switch_to.window(driver.window_handles[1])
-        driver.get("https://jcboseustymca.co.in/Handler/GenerateCaptchaImage.ashx")
-        captcha_element = wait.until(EC.presence_of_element_located((By.TAG_NAME, "img")))
-        screenshot = captcha_element.screenshot_as_png
-        form_entry = form_data(roll_no=current_roll, semester=semester, user=request.user)
-        captcha_file = ContentFile(screenshot, name=f"{current_roll}_{semester}.png")
-        form_entry.captcha.save(f"{current_roll}_{semester}.png", captcha_file, save=False)
-        form_entry.save()
-        data = form_data.objects.filter(id=form_entry.id).values('captcha')
-        image_url = data[0]['captcha']
-        request.session['current_state'] = {
-            'roll_no': current_roll,
-            'end_roll': end_s,
-            'semester': semester,
-            'batch': batch,
-            'branch': branch,
-            'form_entry_id': form_entry.id
-        }
-        driver.switch_to.window(driver.window_handles[0])
-        return render(request, 'scrap/result_scrap.html', {
-            'form_data': form_entry,
-            'current_roll': current_roll,
-            'image_url': image_url
-        })
-    except Exception as e:
-        if driver:
-            driver.quit()
-            driver = None
-        return JsonResponse({'status': 'error', 'message': str(e)})
->>>>>>> 19862dd8e5340800d095c7a479b28c0fa28d6381
 
 @login_required(login_url='login')
 @require_POST
 def submit_captcha(request):
+    """Separate view to handle captcha submission"""
     global driver
-<<<<<<< HEAD
     user = request.user
     if not driver:
-        session = request.session.get('current_state', {})
-        return JsonResponse({'status': 'error', 'message': session})
+        return JsonResponse({'status': 'error', 'message': 'Session expired'})
     
-=======
->>>>>>> 19862dd8e5340800d095c7a479b28c0fa28d6381
     try:
-        if not driver:
-            return JsonResponse({
-                'status': 'error', 
-                'message': 'Browser session expired. Please start over from the beginning.'
-            })
-        
-        captcha_value = request.POST.get('captcha_value', '').upper()
+        captcha_value =request.POST.get('captcha_value')
+        captcha_value = captcha_value.upper()
         current_state = request.session.get('current_state', {})
-        wait = WebDriverWait(driver, 10)
+        
         driver.switch_to.window(driver.window_handles[0])
-        captcha_input = wait.until(EC.presence_of_element_located((By.ID, "txtCaptcha")))
+
+        captcha_input = driver.find_element(By.ID, "txtCaptcha")
         captcha_input.clear()
         captcha_input.send_keys(captcha_value)
-        submit_button = wait.until(EC.element_to_be_clickable((By.ID, "btnResult")))
+        
+        
+        submit_button = driver.find_element(By.ID, "btnResult")
         submit_button.click()
-<<<<<<< HEAD
         
     
         # print("================================================")
@@ -230,60 +178,225 @@ def submit_captcha(request):
         driver.switch_to.window(driver.window_handles[1])
         # print(driver.title)
         # print("-----------------------------------------------")
+        
+        # def subject_parse(soup):
+        #     subject_code= []
+        #     subject_name = []
+        #     credit = []
+        #     theory = []
+        #     sessional = []
+        #     practicle = []
+        #     pass_s = []
+
+        #     for i in range(16 , 150 , 8):
+        #         try:
+        #             table = "".join(soup.select('tr')[i].select('td')[2].text.split())
+        #             subject_code.append(table)
+        #             # print("------------------------------------------------")
+        #             table = " ".join(soup.select('tr')[i].select('td')[3].text.split())
+        #             subject_name.append(table)
+        #             # print("------------------------------------------------")
+        #             table = " ".join(soup.select('tr')[i].select('td')[4].text.split())
+        #             credit.append(table)
+        #             # print("------------------------------------------------")
+        #             table = " ".join(soup.select('tr')[i].select('td')[14].text.split())
+        #             theory.append(table)
+        #             # print("------------------------------------------------")
+        #             table = " ".join(soup.select('tr')[i].select('td')[16].text.split())
+        #             sessional.append(table)
+        #             # print("------------------------------------------------")
+        #             table = " ".join(soup.select('tr')[i].select('td')[18].text.split())
+        #             practicle.append(table)
+        #             # print("------------------------------------------------")
+        #             table = " ".join(soup.select('tr')[i].select('td')[19].text.split())
+        #             pass_s.append(table)
+        #         except: pass
+        #     theory = [x if x != "NA" and x != "" else "0" for x in theory]
+        #     sessional = [x if x != "NA" and x != "" else "0" for x in sessional]
+        #     practicle = [x if x != "NA" and x != "" else "0" for x in practicle]
+        #     total_marks = list(map(lambda x, y , z: int(x) + int(y) +int(z), theory, sessional , practicle))
+            
+
+        #     result_s ={
+        #     "subject_code": subject_code,
+        #     "subject_name": subject_name,
+        #     "credit": credit,
+        #     "theory": theory,
+        #     "sessional": sessional,
+        #     "practicle": practicle,
+        #     "total_marks": total_marks,
+        #     "pass_s": pass_s
+        #     }
+
+        #     result_s = json.dumps(result_s)
+        #     result_s = json.loads(result_s)
+        #     subjects = {}
+            
+        #     for i in range(len(result_s["subject_code"])):
+        #         subject_code = result_s["subject_code"][i]
+        #         subjects[subject_code] = {
+        #             "Subject Name": result_s["subject_name"][i],
+        #             "Credits": result_s["credit"][i],
+        #             "Theory": int(result_s["theory"][i]) if result_s["theory"][i] else 0,
+        #             "Sessional": int(result_s["sessional"][i]),
+        #             "Practical": int(result_s["practicle"][i]) if result_s["practicle"][i] else 0,
+        #             "Total Marks": result_s["total_marks"][i],
+        #             "Grade": result_s["pass_s"][i]
+        #         }
+                
+        #     print(subjects)
+        #     return json.dumps(subjects)
+
+        # def data_save(current_state, page_s , user):
+        #     count = ''
+        #     try:
+        #         batch = current_state['batch']
+        #         branch = current_state['branch']
+        #         roll_no = current_state['roll_no']
+        #         semester_cat = current_state['semester']
+                
+        #         data_id = "_".join([branch ,batch , semester_cat]) 
+                
+        #         soup = BeautifulSoup(page_s, 'html.parser')
+                
+        #         student_name = soup.find(id ="lblname").text
+                
+        #         roll = soup.find(id = "lblRollNo").text
+                
+        #         mother_name = soup.find(id = "lblMotherName").text
+
+        #         father_name = soup.find(id = "lblFatherName").text
+                
+        #         sgpaS = soup.find(id = "lblResult").text
+        #         re_appear_count = ''
+
+        #         try :
+        #             if isinstance(float(sgpaS) , float):
+        #                 print("True integer")
+        #                 sgpa = float(sgpaS)
+        #                 re_appear_count = 0
+        #                 print(re_appear_count , sgpaS)
+        #         except:
+        #             sgpaS = sgpaS.split()
+        #             re_appear_count = len(sgpaS)
+        #             print(re_appear_count , sgpaS)
+        #             print("Not an integer")
+                            
+        #         c_result = soup.find(id = "lblCgpaResult").text
+                
+        #         print("Here before process_Sub_result")
+                
+        #         #EVERYTHING FINE TILL HERE
+        #         sub_result =  json.loads(subject_parse(soup))
+        #         print("Here after process_sgpa")
+                
+        #         print("here " , student_name,roll,father_name,mother_name,data_id,sgpaS,c_result ,re_appear_count , sub_result)
+        #         print("Here after process_sgpa")
+                
+        #         result_entry = result.objects.filter(category = data_id , user_id=request.user , roll_no = roll)
+        #         print(result_entry)
+                
+        #         if result_entry.exists(): 
+        #             print("Matching Found UPDATING !!")        
+        #             result_entry = result(s_name=student_name,roll_no=roll,f_name=father_name,m_name=mother_name,category=data_id, sgpa=sgpaS ,cgpa=c_result ,re_count=re_appear_count ,result_s = sub_result , user =user)
+        #             result_entry.save()
+
+        #         else: 
+        #             print("Matching not Found")        
+        #             result_entry = result(s_name=student_name,roll_no=roll,f_name=father_name,m_name=mother_name,category=data_id, sgpa=sgpaS ,cgpa=c_result ,re_count=re_appear_count ,result_s = sub_result , user =user)
+        #             result_entry.save()
+        #             print("Hererererer")
+                
+        #         print("saving funcitn call")
+        #         count = 0
+        #     except :
+        #         count = 1
+        #     return count
+                
+        # def image_rename(id , captcha_value):
+        #     try :
+        #         form = form_data.objects.get(id=id)
+            
+        #         old_file_path = os.path.join(media_path , f"{form.captcha}")
+                
+        #         new_file_name = f"{captcha_value}.png"  # Make sure to 
+        #         new_file_path = os.path.join(os.path.dirname(old_file_path), new_file_name)
+        #         os.rename(old_file_path, new_file_path)
+                
+        #         form.captcha = f"images/{captcha_value}.png"
+        #         form.save()
+        #     except:
+        #         print("In image_rename")
+    
                    
         if len(driver.window_handles)==3:
-=======
-        if len(driver.window_handles) == 3:
->>>>>>> 19862dd8e5340800d095c7a479b28c0fa28d6381
             driver.switch_to.window(driver.window_handles[2])
-            data_save(current_state, driver.page_source, request.user)
+            
+            data_save(current_state , driver.page_source , user)   
             image_rename2(current_state['form_entry_id'], captcha_value)
-            driver.close()
+         
+            driver.close()       
+                   
+        # Process result page he
+        # Update roll number
+        
         next_roll = str(int(current_state['roll_no']) + 1)
+        # print("Next New Roll No : ", next_roll)
         
         if int(next_roll) >= int(current_state['end_roll']):
             driver.quit()
             driver = None
             return JsonResponse({'status': 'completed'})
-            
+        
+        # Update session with new roll number
         current_state['roll_no'] = next_roll
         request.session['current_state'] = current_state
-        request.session.modified = True
+        request.session.modified = True  # Force session update
         
+        # Close extra windows and prepare for next roll
         driver.switch_to.window(driver.window_handles[1])
-        a = driver.title
         driver.close()
         driver.switch_to.window(driver.window_handles[0])
-        b = driver.title
+        
+
+        # Return success with next roll number
         return JsonResponse({
-            'status': 'success',
-            'message': f"driver name {a ,b }",
+            'status': 'success', 
             'next_roll': next_roll,
-            'batch': current_state['batch'],
-            'redirect_url': f'/profile/scrap/results/{next_roll}/{current_state["end_roll"]}/{current_state["semester"]}/{current_state["batch"]}/{current_state["branch"]}'
+            'batch' : current_state['batch'],
+            'redirect_url': f'/profile/scrap/results/{next_roll}/{current_state["end_roll"]}/{current_state["semester"]}/{current_state['batch']}/{current_state['branch']}'
         })
+        
+        
     except Exception as e:
-        if driver:
-            try:
-                driver.quit()
-            except:
-                pass
-            driver = None
+        # print("InLast exception")
         return JsonResponse({'status': 'error', 'message': str(e)})
+
+
 
 def json_trial(request):
     if request.method == 'POST':
         name = request.POST.get('name')
         roll_no = request.POST.get('roll')
-        json_data_str = request.POST.get('json_data')
+        json_data_str = request.POST.get('json_data')  # Fix the name to match the form
+
         try:
+            # Parse JSON data
             json_data = json.loads(json_data_str)
-            trail_json_instance = trail_json.objects.create(name=name, roll_no=roll_no, result=json_data)
+            
+            # Save to the database
+            trail_json_instance = trail_json.objects.create(
+                name=name,
+                roll_no=roll_no,
+                result=json_data
+            )
+
             print(name, roll_no, json_data)
             return JsonResponse({'message': 'Data saved successfully!', 'id': trail_json_instance.id})
         except json.JSONDecodeError:
             return JsonResponse({'error': 'Invalid JSON data'}, status=400)
+
     return render(request, "scrap/trail_json.html")
 
 def checkhtml(request):
-    return render(request, "scrap/temp_database.html")
+    return render(request , "scrap/temp_database.html") 
