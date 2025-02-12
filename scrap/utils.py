093d8b8c7b2cd5974098171bfb53d599603b7830
Priyanshu Choubey <83550858+priyans877@users.noreply.github.com>
2025-02-12 20:51:52 +0530

    Update utils.py

--- scrap/utils.py
+++ scrap/utils.py
@@ -190,8 +190,8 @@ def image_rename2(id, captcha_value):
         # Initialize S3 client
         s3_client = boto3.client(
             's3',
-            aws_access_key_id=os.getenv('L_AWS_ACCESS_KEY_ID'),
-            aws_secret_access_key=os.getenv('L_AWS_SECRET_ACCESS_KEY'),
+            aws_access_key_id=os.getenv('AWS_ACCESS_KEY_ID'),
+            aws_secret_access_key=os.getenv('AWS_SECRET_ACCESS_KEY'),
             region_name=os.getenv('AWS_S3_REGION_NAME')
         )
         sts_client = boto3.client('sts')
